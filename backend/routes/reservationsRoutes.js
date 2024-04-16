967821a803a9a49d6f6e8f38509c7bb41597575e
IvanGM <ivangm_01@hotmail.com>
2024-04-16 01:20:35 -0600

    feat: Funcionalidades de admin agregadas

--- backend/routes/reservationsRoutes.js
+++ backend/routes/reservationsRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const router = Router()
-const { createReservation, makeDeposit, chooseSeats, makeDevolution, changeConfirmedSeats } = require('@/controllers/reservationsControllers/generalControllers')
+const { createReservation, makeDeposit, chooseSeats, makeDevolution, changeConfirmedSeats, reduceReservedSeatsAmount } = require('@/controllers/reservationsControllers/generalControllers')
 const { addDiscount, reducePendingDevolution, reduceAmountPaid } = require('@/controllers/reservationsControllers/adminControllers')
 const { protect, adminProtect } = require('@/middleware/authMiddleware')
 
@@ -8,6 +8,7 @@ router.post('/create/:id/:phone', protect, createReservation)
 router.post('/deposit/:id', protect, makeDeposit)
 router.post('/seats/select/:id', protect, chooseSeats)
 router.post('/seats/change/:id', protect, changeConfirmedSeats)
+router.post('/seats/reduce-amount/:id', protect, reduceReservedSeatsAmount)
 router.post('/devolution/:id', protect, makeDevolution)
 
 router.post('/admin/discount/:id', protect, adminProtect, addDiscount)
